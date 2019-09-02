fcda06f1714af8c0b492ff61b21c3993c9d8f46f
Elizabeth Ford <daughterofspring@DaughteofSpring.attlocal.net>
2019-09-02 15:07:48 -0400

    Changed to LocalTime and started work to find pay from 5 to bedtime

--- src/main/java/com/bethford/babysitterkata/BabysitterPay.java
+++ src/main/java/com/bethford/babysitterkata/BabysitterPay.java
@@ -1,28 +1,78 @@
 package com.bethford.babysitterkata;
 
+import java.time.LocalTime;
+import static java.time.temporal.ChronoUnit.MINUTES;
+
 public class BabysitterPay {
 	
 	private int totalDailyPay;
-	private String bedtime;
+	private LocalTime bedtime;
 	
-	private int earliestStart = 17;
-	private int latestEnd = 4;
+	
+	// Hard-coded variables for pay and cut-of times
+	private LocalTime earliestStart = LocalTime.of(17,0);
+	private LocalTime latestEnd = LocalTime.of(4, 0);
 	private int standardRate = 12;
 	private int bedTimeToMidnightRate = 8;
-	private int midnightTo4Rate = 16;
+	private int midnightToFourRate = 16;
 	
 
 	public BabysitterPay(String bedtime) {
-		this.bedtime = bedtime;
+		this.bedtime = convertStringTimeToLocalTime(bedtime);
 	}
 	
 	
 	public int calculateTotalDailyPay(String startTime, String endTime) {
+		LocalTime start = convertStringTimeToLocalTime(startTime);
+		LocalTime end = convertStringTimeToLocalTime(endTime);
 		
+		int standardPayForTheDay = 0;
+		int bedtimeToMidnightPayForTheDay = 0;
+		int midnightToFourPayForTheDay = 0;
 		
+		standardPayForTheDay = findStandardPayAmount(start, end);
+
+		totalDailyPay = standardPayForTheDay + bedtimeToMidnightPayForTheDay + midnightToFourPayForTheDay;
 		
 		return totalDailyPay;
 	}
 	
+	private int findStandardPayAmount(LocalTime startTime, LocalTime endTime) {
+		int hoursWorked = 0;
+		if((startTime.isBefore(earliestStart) && startTime.isAfter(latestEnd))
+				|| (startTime.isBefore(earliestStart) && startTime.isAfter(LocalTime.of(0, 0)) && startTime.isAfter(endTime))) {
+			startTime = earliestStart;
+			if(endTime.isBefore(bedtime)) {
+				hoursWorked = Math.round(MINUTES.between(startTime, endTime) / 60);
+			}
+			else {
+				hoursWorked = Math.round(MINUTES.between(startTime, bedtime) / 60);
+			}
+		}
+		return 12 * hoursWorked;
+	}
+	
+	
+
+	
+	private LocalTime convertStringTimeToLocalTime(String time) {
+		String[] timeArray = time.substring(0, time.length() - 2).split(":");
+		int hour = getMilitaryHourFromString(time, timeArray);
+		int minutes = Integer.parseInt(timeArray[1]);
+		return LocalTime.of(hour, minutes);
+	}
+	
+	private int getMilitaryHourFromString(String time, String[] timeArray) {
+		if(time.substring(time.length() - 2).contentEquals("AM")) {
+			if(timeArray[0].equals("12")){
+				return 0;
+			}
+			return Integer.parseInt(timeArray[0]);
+		}
+		else {
+			return Integer.parseInt(timeArray[0]) + 12;
+		}
+	}
+
 
 }
--- src/test/java/com/bethford/babysitterkata/BabysitterPayUnitTest.java
+++ src/test/java/com/bethford/babysitterkata/BabysitterPayUnitTest.java
@@ -12,119 +12,119 @@ public class BabysitterPayUnitTest {
 	
 	@Before
 	public void setup() {
-		this.testPay = new BabysitterPay("8:00 PM");
+		this.testPay = new BabysitterPay("8:00PM");
 	}
 	
 	
 	// Test that no work returns no pay. //
 	@Test
 	public void no_hours_worked_zero_pay () {
-		int result = testPay.calculateTotalDailyPay("5:00 PM", "5:00 PM");
+		int result = testPay.calculateTotalDailyPay("5:00PM", "5:00PM");
 		Assert.assertEquals(0, result);
 	}
 	
 	// Test that hours worked outside of the allowable time returns no pay. //
 	@Test
 	public void hours_input_not_allowed_no_pay() {
-		int result = testPay.calculateTotalDailyPay("12:00 PM", "5:00 PM");
+		int result = testPay.calculateTotalDailyPay("12:00PM", "5:00PM");
 		Assert.assertEquals(0, result);
 	}
 
 	// Test that hours partially outside allowable hours returns pay only for allowable time. //
 	@Test
 	public void hours_input_only_partly_in_allowable_range() {
-		int result = testPay.calculateTotalDailyPay("12:00PM", "6:00 PM");
+		int result = testPay.calculateTotalDailyPay("12:00PM", "6:00PM");
 		Assert.assertEquals(12, result);
 	}
 	
 	// Test for hours before bedtime ONLY with 8PM bedtime. //
 	@Test
 	public void twelve_per_hour_range_only_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("5:00 PM", "8:00 PM");
+		int result = testPay.calculateTotalDailyPay("5:00PM", "8:00PM");
 		Assert.assertEquals(36, result);
 	}
 	
 	// Test for hours after bedtime and before midnight ONLY with 8PM bedtime. //
 	@Test
 	public void eight_per_hour_range_only_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("8:00 PM", "10:00 PM");
+		int result = testPay.calculateTotalDailyPay("8:00PM", "10:00PM");
 		Assert.assertEquals(16, result);
 	}
 	
 	// Test for hours after midnight ONLY with 8PM bedtime. //
 	@Test
 	public void sixteen_per_hour_range_only_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("12:00 AM", "2:00 AM");
+		int result = testPay.calculateTotalDailyPay("12:00AM", "2:00AM");
 		Assert.assertEquals(32, result);
 	}
 	
 	// Test for hours before 5PM to after 8PM. //
 	@Test
 	public void mixed_regular_low_and_out_of_bounds_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("12:00 PM", "9:00 PM");
+		int result = testPay.calculateTotalDailyPay("12:00PM", "9:00PM");
 		Assert.assertEquals(44, result);
 	}
 	
 	// Test for hours after bedtime to after midnight. //
 	@Test
 	public void after_bedtime_to_after_midnight_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("8:00 PM", "1:00 AM");
+		int result = testPay.calculateTotalDailyPay("8:00PM", "1:00AM");
 		Assert.assertEquals(48, result);
 	}
 	
 	// Test for hours before 5PM to after midnight. //
 	@Test
 	public void mixed_regular_low_late_and_out_of_bounds_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("12:00 PM", "1:00 AM");
+		int result = testPay.calculateTotalDailyPay("12:00PM", "1:00AM");
 		Assert.assertEquals(92, result);
 	}
 	
 	// Test for hours before 5PM to after 8PM. //
 	@Test
 	public void mixed_regular_low_and_late_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("5:00 PM", "1:00 AM");
+		int result = testPay.calculateTotalDailyPay("5:00PM", "1:00AM");
 		Assert.assertEquals(92, result);
 	}
 	
 	// Test for 15 minutes in standard hours and rate. //
 	@Test
 	public void standard_quarter_hour_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("5:00 PM", "5:15 PM");
+		int result = testPay.calculateTotalDailyPay("5:00PM", "5:15PM");
 		Assert.assertEquals(12, result);
 	}
 	
 	// Test for 15 minutes in after bedtime hours and rate. //
 	@Test
 	public void bedtime_quarter_hour_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("8:00 PM", "8:15 PM");
+		int result = testPay.calculateTotalDailyPay("8:00PM", "8:15PM");
 		Assert.assertEquals(8, result);
 	}
 
 	// Test for 15 minutes in after midnight hours and rate. //
 	@Test
 	public void after_midnight_quarter_hour_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("12:00 AM", "12:15 AM");
+		int result = testPay.calculateTotalDailyPay("12:00AM", "12:15AM");
 		Assert.assertEquals(16, result);
 	}
 	
 	// Test for fraction hours in standard and post bedtime hours and rate. //
 	@Test
 	public void partial_standard_and_partial_post_bedtime_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("5:30 PM", "8:45 PM");
+		int result = testPay.calculateTotalDailyPay("5:30PM", "8:45PM");
 		Assert.assertEquals(44, result);
 	}
 	
 	// Test for fraction hours in standard and post midnight hours and rate. //
 	@Test
 	public void partial_standard_and_partial_post_midnight_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("5:30 PM", "12:45 AM");
+		int result = testPay.calculateTotalDailyPay("5:30PM", "12:45AM");
 		Assert.assertEquals(92, result);
 	}
 	
 	// Test for fraction hours in post bedtime and post midnight hours and rate. //
 	@Test
 	public void partial_post_bedtime_and_partial_post_midnight_calculates_correctly() {
-		int result = testPay.calculateTotalDailyPay("8:30 PM", "12:45 AM");
+		int result = testPay.calculateTotalDailyPay("8:30PM", "12:45AM");
 		Assert.assertEquals(48, result);
 	}
 }
